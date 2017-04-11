f332ce4e35ac71c430a3d995e1edb45ee7cfb5d6
Unknown <jonathangreer80@outlook.com>
2017-04-11 14:30:35 -0500

    Initial Commit
    setting up database entities

---
+++ src/main/java/com/cooksys/entity/FlightEntity.java
@@ -0,0 +1,81 @@
+package com.cooksys.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class FlightEntity {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String origin;
+	
+	private String destination;
+	
+	private Long offset;
+	
+	public FlightEntity() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+	public Long getOffset() {
+		return offset;
+	}
+
+	public void setOffset(Long offset) {
+		this.offset = offset;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FlightEntity other = (FlightEntity) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
---
+++ src/main/java/com/cooksys/entity/Itinerary.java
@@ -0,0 +1,85 @@
+package com.cooksys.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Itinerary {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@OneToMany
+	private List<FlightEntity> flights;
+	
+	private Long flightTime;
+	
+	private Long layover;
+
+	public Itinerary() {
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<FlightEntity> getFlights() {
+		return flights;
+	}
+
+	public void setFlights(List<FlightEntity> flights) {
+		this.flights = flights;
+	}
+
+	public Long getFlightTime() {
+		return flightTime;
+	}
+
+	public void setFlightTime(Long flightTime) {
+		this.flightTime = flightTime;
+	}
+
+	public Long getLayover() {
+		return layover;
+	}
+
+	public void setLayover(Long layover) {
+		this.layover = layover;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Itinerary other = (Itinerary) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+}
